43f0547a7f6556b5fab6ba654d9150eb086a5dff
Noah Zoschke <noah@heroku.com>
2010-02-22 16:29:48 -0800

    samorau manifest, heroku provision and deprovision handlers

---
+++ heroku/handlers.py
@@ -0,0 +1,29 @@
+from google.appengine.ext import webapp
+from django.utils import simplejson
+from decorators import http_auth_required
+from errorbucket.buckets.models import Bucket
+
+CREDENTIALS = {'heroku': 'cf6d0d056bb08b03'}
+
+class ResourcesHandler(webapp.RequestHandler):
+  @http_auth_required(CREDENTIALS)
+  def post(self):
+    bucket = Bucket.create()
+    result = { 'id': bucket.key().name(),
+      'config': { 
+        'ERRORBUCKET_URL': "http://%s/buckets/%s" % (self.request.headers['HOST'], bucket.key().name()),
+        'ERRORBUCKET_SECRET_KEY': bucket.secret_key
+      }
+    }
+    self.response.out.write(simplejson.dumps(result))
+    self.response.headers.add_header("Content-Type", "application/json")
+
+class ResourceHandler(webapp.RequestHandler):
+  @http_auth_required(CREDENTIALS)
+  def delete(self, key_name):
+    bucket = Bucket.get_by_key_name(key_name)
+    if bucket:
+      bucket.delete()
+      self.response.out.write('ok')
+    else:
+      self.response.set_status(404)
---
+++ heroku/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+from heroku.handlers import ResourcesHandler, ResourceHandler
+  
+def main():
+  application = webapp.WSGIApplication([
+    ('/heroku/resources', ResourcesHandler),
+    ('/heroku/resources/(.*?)', ResourceHandler),
+  ], debug=True)
+  util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
+
